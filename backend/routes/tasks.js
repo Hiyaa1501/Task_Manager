40d5373ff872dc9a5347e53856a5135d725116e1
hiyaa1501 <hiyabhakta@gmail.com>
2026-04-01 15:00:01 +0530

    Final cleanup of middleware require paths

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Task = require("../models/Task"); 
-const auth = require('../middleware/authMiddleware'); 
+const auth = require('../middleware/authMiddleware');
 
 // GET ALL USER TASKS
 router.get("/", auth, async (req, res) => {
