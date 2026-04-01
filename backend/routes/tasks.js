8d83424e3b7ea8f03f2e2ffba9b73aacedb5b0d1
hiyaa1501 <hiyabhakta@gmail.com>
2026-04-01 14:52:08 +0530

    Finalizing Task require path in routes

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Task = require("../models/Tasks"); 
+const Task = require("../models/Task"); 
 const auth = require('../middleware/authMiddleware'); 
 
 // GET ALL USER TASKS
